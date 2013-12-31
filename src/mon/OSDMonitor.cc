9cdacf5639e62af00a2cc86db2581b174c9f1060
James Page <james.page@ubuntu.com>
2013-12-31 14:37:29 +0000

    Imported Upstream version 0.72.2

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -2734,22 +2734,19 @@ int OSDMonitor::prepare_command_pool_set(map<string,cmd_vartype> &cmdmap,
   if (pending_inc.new_pools.count(pool))
     p = pending_inc.new_pools[pool];
 
-  // accept val as a json string or int, and parse out int or float
+  // accept val as a json string or int, and parse out int
   // values from the string as needed
+  string raw_val = cmd_vartype_stringify(cmdmap["val"]);
   string val;
   cmd_getval(g_ceph_context, cmdmap, "val", val);
   string interr;
   int64_t n = 0;
   if (!cmd_getval(g_ceph_context, cmdmap, "val", n))
     n = strict_strtoll(val.c_str(), 10, &interr);
-  string floaterr;
-  float f;
-  if (!cmd_getval(g_ceph_context, cmdmap, "val", f))
-    f = strict_strtod(val.c_str(), &floaterr);
 
   if (var == "size") {
     if (interr.length()) {
-      ss << "error parsing integer value '" << val << "': " << interr;
+      ss << "error parsing integer value '" << raw_val << "': " << interr;
       return -EINVAL;
     }
     if (n == 0 || n > 10) {
@@ -2762,21 +2759,21 @@ int OSDMonitor::prepare_command_pool_set(map<string,cmd_vartype> &cmdmap,
     ss << "set pool " << pool << " size to " << n;
   } else if (var == "min_size") {
     if (interr.length()) {
-      ss << "error parsing integer value '" << val << "': " << interr;
+      ss << "error parsing integer value '" << raw_val << "': " << interr;
       return -EINVAL;
     }
     p.min_size = n;
     ss << "set pool " << pool << " min_size to " << n;
   } else if (var == "crash_replay_interval") {
     if (interr.length()) {
-      ss << "error parsing integer value '" << val << "': " << interr;
+      ss << "error parsing integer value '" << raw_val << "': " << interr;
       return -EINVAL;
     }
     p.crash_replay_interval = n;
     ss << "set pool " << pool << " to crash_replay_interval to " << n;
   } else if (var == "pg_num") {
     if (interr.length()) {
-      ss << "error parsing integer value '" << val << "': " << interr;
+      ss << "error parsing integer value '" << raw_val << "': " << interr;
       return -EINVAL;
     }
     if (n <= (int)p.get_pg_num()) {
@@ -2795,7 +2792,7 @@ int OSDMonitor::prepare_command_pool_set(map<string,cmd_vartype> &cmdmap,
     }
   } else if (var == "pgp_num") {
     if (interr.length()) {
-      ss << "error parsing integer value '" << val << "': " << interr;
+      ss << "error parsing integer value '" << raw_val << "': " << interr;
       return -EINVAL;
     }
     if (n <= 0) {
@@ -2816,7 +2813,7 @@ int OSDMonitor::prepare_command_pool_set(map<string,cmd_vartype> &cmdmap,
     }
   } else if (var == "crush_ruleset") {
     if (interr.length()) {
-      ss << "error parsing integer value '" << val << "': " << interr;
+      ss << "error parsing integer value '" << raw_val << "': " << interr;
       return -EINVAL;
     }
     if (osdmap.crush->rule_exists(n)) {
@@ -2827,14 +2824,16 @@ int OSDMonitor::prepare_command_pool_set(map<string,cmd_vartype> &cmdmap,
       return -ENOENT;
     }
   } else if (var == "hashpspool") {
-    if (val == "true") {
+    // make sure we only compare against 'n' if we didn't receive a string
+    int flag_val = (!val.empty() ? -1 : n);
+    if (val == "true" || flag_val == 1) {
       p.flags |= pg_pool_t::FLAG_HASHPSPOOL;
       ss << "set";
-    } else if (val == "false") {
+    } else if (val == "false" || flag_val == 0) {
       p.flags ^= pg_pool_t::FLAG_HASHPSPOOL;
       ss << "unset";
     } else {
-      ss << "expecting value true or false";
+      ss << "expecting value 0 or 1";
       return -EINVAL;
     }
     ss << " pool " << pool << " flag hashpspool";
