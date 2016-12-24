0f1cf47f093d49305df277e3f367e895fcd94b22
Alexey Sheplyakov <asheplyakov@mirantis.com>
2016-12-24 16:23:46 +0400

    Adjust packaging for Ubuntu 12.04

--- debian/pkgbuild.sh
+++ debian/pkgbuild.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 repo=/srv/data/Public/repos/ceph
-dist=trusty
+dist=precise
 ceph_release=hammer
+build_chroot='mos60'
 export_dir="../build-pkg-ceph-${ceph_release}-${dist}"
 if [ ! -d "$export_dir" ]; then mkdir -p "$export_dir"; fi
 
@@ -12,5 +13,5 @@ exec gbp buildpackage \
         --git-pristine-tar-commit \
         --git-export-dir="$export_dir" \
         --git-cleaner='git clean -dfx' \
-        --git-builder="sbuild -v --dist=${dist} --post-build-commands \"reprepro -Vb${repo} --ignore=wrongdistribution --ignore=missingfile include ${ceph_release}-${dist} %SBUILD_CHANGES\"" \
+        --git-builder="sbuild -v --dist=${build_chroot} --post-build-commands \"reprepro -Vb${repo} --ignore=wrongdistribution --ignore=missingfile include ${ceph_release}-${dist} %SBUILD_CHANGES\"" \
 	$@
