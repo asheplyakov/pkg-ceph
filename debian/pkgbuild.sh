96936f6e974b4f6ad5ad0e14be50b420f12f1490
Alexey Sheplyakov <asheplyakov@mirantis.com>
2017-02-10 16:20:48 +0400

    d/pkgbuild.sh: set ceph version string to match the package version
    (cherry picked from commit cc8a4e10a1100620e0ffb438f2eac44b238dce15)

--- debian/pkgbuild.sh
+++ debian/pkgbuild.sh
@@ -11,6 +11,16 @@ exec gbp buildpackage \
         --git-pristine-tar \
         --git-pristine-tar-commit \
         --git-export-dir="$export_dir" \
+	--git-postexport='set -e;
+		( git --git-dir=$GBP_GIT_DIR rev-parse HEAD &&
+		  ver=`dpkg-parsechangelog -l$GBP_TMP_DIR/debian/changelog --show-field Version`;
+		  echo "v${ver}";
+		) > $GBP_TMP_DIR/src/.git_version;
+		' \
         --git-cleaner='git clean -dfx' \
+	--git-prebuild='set -e;
+		cd $GBP_BUILD_DIR;
+		dpkg-source --commit . decapod_ceph_version;
+		' \
         --git-builder="sbuild -v --dist=${dist} --post-build-commands \"reprepro -Vb${repo} --ignore=wrongdistribution --ignore=missingfile include ${ceph_release}-${dist} %SBUILD_CHANGES\"" \
 	$@
